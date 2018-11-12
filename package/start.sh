1c3b36312fff5d9a4d9a428927a385571dcac22b
nalbam <me@nalbam.com>
2018-11-12 12:39:55 +0900

    start

--- package/start.sh
+++ package/start.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 if [ -f /home/pi/.config/rpi-run ]; then
-    $(cat /home/pi/.config/rpi-run)
+    KIOSK=$(cat /home/pi/.config/rpi-run | xargs)
+
+    if [ "${KIOSK}" != "" ]; then
+        $(${KIOSK})
+    fi
 fi
 
-KIOSK=$(cat /home/pi/.config/rpi-kiosk | xargs)
+if [ -f /home/pi/.config/rpi-kiosk ]; then
+    KIOSK=$(cat /home/pi/.config/rpi-kiosk | xargs)
 
-if [ "${KIOSK}" != "" ]; then
-    unclutter &
-    matchbox-window-manager &
-    chromium-browser --incognito --kiosk ${KIOSK}
+    if [ "${KIOSK}" != "" ]; then
+        unclutter &
+        matchbox-window-manager &
+        chromium-browser --incognito --kiosk ${KIOSK}
+    fi
 fi
