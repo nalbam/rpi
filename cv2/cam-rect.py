99498cb05f4cd691f465c9672b79676520da270d
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2020-02-24 00:42:25 +0900

    add argparse

--- cv2/cam-rect.py
+++ cv2/cam-rect.py
@@ -1,41 +1,70 @@
+import argparse
 import cv2
 
-# Get a reference to webcam #0 (the default one)
-cap = cv2.VideoCapture(0)
 
-frame_w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-frame_h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+def parse_args():
+    p = argparse.ArgumentParser(description="webcam demo")
+    p.add_argument("--camera-id", type=int, default=0, help="camera id")
+    p.add_argument("--full-screen", action="store_true", help="full screen")
+    p.add_argument("--mirror", action="store_true", help="mirror")
+    p.add_argument("--width", type=int, default=0, help="width")
+    p.add_argument("--height", type=int, default=0, help="height")
+    return p.parse_args()
 
-rate = 8 / frame_h
-bigg = frame_h / 8
 
-while True:
-    # Grab a single frame of video
-    ret, frame = cap.read()
+def main():
+    args = parse_args()
 
-    # overlay
-    overlay = frame.copy()
+    # Get a reference to webcam #0 (the default one)
+    cap = cv2.VideoCapture(0)
 
-    # rectangle
-    color = (0, 0, 255)
-    left = int(6 * bigg)
-    right = int(7 * bigg)
-    top = int(4 * bigg)
-    bottom = int(5 * bigg)
-    cv2.rectangle(overlay, (left, top), (right, bottom), color, cv2.FILLED)
+    frame_w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    frame_h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-    cv2.addWeighted(overlay, 0.3, frame, 1 - 0.4, 0, frame)
+    print(frame_w, frame_h)
+    print('Press "Esc", "q" or "Q" to exit.')
 
-    # Display the resulting image
-    cv2.imshow("Video", frame)
+    rate = 8 / frame_h
+    bigg = frame_h / 8
 
-    cv2.namedWindow("Video", cv2.WINDOW_NORMAL)
-    cv2.setWindowProperty("Video", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    while True:
+        # Grab a single frame of video
+        ret, frame = cap.read()
 
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+        # overlay
+        overlay = frame.copy()
 
-# Release handle to the webcam
-cap.release()
-cv2.destroyAllWindows()
+        # rectangle
+        color = (0, 0, 255)
+        left = int(6 * bigg)
+        right = int(7 * bigg)
+        top = int(4 * bigg)
+        bottom = int(5 * bigg)
+        cv2.rectangle(overlay, (left, top), (right, bottom), color, cv2.FILLED)
+
+        cv2.addWeighted(overlay, 0.3, frame, 1 - 0.4, 0, frame)
+
+        # Invert left and right
+        frame = cv2.flip(frame, 1)
+
+        # Display the resulting image
+        cv2.imshow("Video", frame)
+
+        cv2.namedWindow("Video", cv2.WINDOW_NORMAL)
+
+        if args.full_screen:
+            cv2.setWindowProperty(
+                "Video", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN
+            )
+
+        ch = cv2.waitKey(1)
+        if ch == 27 or ch == ord("q") or ch == ord("Q"):
+            break
+
+    # Release handle to the webcam
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
--- cv2/cam.py
+++ cv2/cam.py
@@ -1,27 +1,83 @@
+import argparse
 import cv2
 
-# Get a reference to webcam #0 (the default one)
-cap = cv2.VideoCapture(0)
 
-frame_w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-frame_h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+def parse_args():
+    p = argparse.ArgumentParser(description="webcam demo")
+    p.add_argument("--camera-id", type=int, default=0, help="camera id")
+    p.add_argument("--full-screen", action="store_true", help="full screen")
+    p.add_argument("--mirror", action="store_true", help="mirror")
+    p.add_argument("--width", type=int, default=0, help="width")
+    p.add_argument("--height", type=int, default=0, help="height")
+    p.add_argument("--crop", action="store_true", help="crop")
+    p.add_argument("--mosaic", action="store_true", help="mosaic")
+    return p.parse_args()
 
-print(frame_w, frame_h)
 
-while True:
-    # Grab a single frame of video
-    ret, frame = cap.read()
+def main():
+    args = parse_args()
 
-    # Display the resulting image
-    cv2.imshow("Video", frame)
+    # Get a reference to webcam #0 (the default one)
+    cap = cv2.VideoCapture(args.camera_id)
 
-    cv2.namedWindow("Video", cv2.WINDOW_NORMAL)
-    cv2.setWindowProperty("Video", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    if args.width > 0 and args.height > 0:
+        frame_w = args.width
+        frame_h = args.height
+    else:
+        frame_w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        frame_h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+    print(frame_w, frame_h)
+    print('Press "Esc", "q" or "Q" to exit.')
 
-# Release handle to the webcam
-cap.release()
-cv2.destroyAllWindows()
+    while True:
+        # Grab a single frame of video
+        ret, frame = cap.read()
+
+        # print("Resolution: " + str(frame.shape[0]) + " x " + str(frame.shape[1]))
+
+        if args.crop:
+            w = int(frame_w / 2)
+            x = int(w / 2)
+            y = int((frame_h - w) / 2)
+
+            # Crop square
+            frame = frame[y : y + w, x : x + w]
+
+        if args.mosaic:
+            rate = 8 / frame_h
+            bigg = frame_h / 8
+
+            # Resize to 8x8
+            frame = cv2.resize(frame, (0, 0), fx=rate, fy=rate)
+
+            # Resize to orignal
+            frame = cv2.resize(
+                frame, (0, 0), fx=bigg, fy=bigg, interpolation=cv2.INTER_AREA
+            )
+
+        if args.mirror:
+            # Invert left and right
+            frame = cv2.flip(frame, 1)
+
+        # Display the resulting image
+        cv2.imshow("Video", frame)
+
+        cv2.namedWindow("Video", cv2.WINDOW_NORMAL)
+
+        if args.full_screen:
+            cv2.setWindowProperty(
+                "Video", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN
+            )
+
+        ch = cv2.waitKey(1)
+        if ch == 27 or ch == ord("q") or ch == ord("Q"):
+            break
+
+    # Release handle to the webcam
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
