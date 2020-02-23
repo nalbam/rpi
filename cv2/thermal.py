0b10e47ad3aea2336e43faf1559ced39cdb38608
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2020-02-24 00:46:50 +0900

    add Sensor class

--- cv2/thermal.py
+++ cv2/thermal.py
@@ -1,3 +1,4 @@
+import argparse
 import cv2
 import math
 import numpy as np
@@ -15,132 +16,158 @@ MAXTEMP = 30.0
 # how many color values we can have
 COLORDEPTH = 1024
 
-BORDER = (100, 100, 100)
 
-root = tk.Tk()
-
-# SCREEN_W = root.winfo_screenwidth()  # 480  # cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-# SCREEN_H = root.winfo_screenheight()  # 320  # cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-
-# print(SCREEN_W, SCREEN_H)
-
-# Get a reference to webcam #0 (the default one)
-cap = cv2.VideoCapture(0)
-
-SCREEN_W = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-SCREEN_H = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-
-# SCREEN_W = root.winfo_screenwidth()  # 480  # cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-# SCREEN_H = root.winfo_screenheight()  # 320  # cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-
-print(SCREEN_W, SCREEN_H)
-
-BOX_SIZE = [int(SCREEN_W / 2), int(SCREEN_W / 2)]
-BOX_PIX = [BOX_SIZE[0] / 32, BOX_SIZE[1] / 32]
-BOX_POS = [int((SCREEN_W - BOX_SIZE[0]) / 2), int((SCREEN_H - BOX_SIZE[1]) / 2)]
-
-# pylint: disable=invalid-slice-index
-points = [(math.floor(ix / 8), (ix % 8)) for ix in range(0, 64)]
-grid_x, grid_y = np.mgrid[0:7:32j, 0:7:32j]
-# pylint: enable=invalid-slice-index
-
-# the list of colors we can choose from
-blue = Color("indigo")
-colors = list(blue.range_to(Color("red"), COLORDEPTH))
-
-# create the array of colors
-colors = [(int(c.red * 255), int(c.green * 255), int(c.blue * 255)) for c in colors]
+def parse_args():
+    p = argparse.ArgumentParser(description="webcam demo")
+    p.add_argument("--camera-id", type=int, default=0, help="camera id")
+    p.add_argument("--full-screen", action="store_true", help="full screen")
+    p.add_argument("--mirror", action="store_true", help="mirror")
+    p.add_argument("--width", type=int, default=0, help="width")
+    p.add_argument("--height", type=int, default=0, help="height")
+    p.add_argument("--min-temp", type=float, default=MINTEMP, help="height")
+    p.add_argument("--max-temp", type=float, default=MAXTEMP, help="height")
+    return p.parse_args()
 
 
-# some utility functions
-def get_position(i, j):
-    pt1 = (
-        int((BOX_PIX[0] * i)),
-        int((BOX_PIX[1] * j) + BOX_POS[1]),
-    )
-    pt2 = (
-        int((BOX_PIX[0] * (i + 1))),
-        int((BOX_PIX[1] * (j + 1)) + BOX_POS[1]),
-    )
-    return pt1, pt2
+# def draw_video(frame):
+#     # move video
+#     w = int(SCREEN_W / 4)
+#     M = [[1, 0, w], [0, 1, 0]]
+
+#     h, w = frame.shape[:2]
+#     M = np.float32(M)
+#     frame = cv2.warpAffine(frame, M, (w, h))
 
 
-def get_color(val):
-    # i = min(COLORDEPTH - 1, max(0, int(val)))
-    i = COLORDEPTH - min(COLORDEPTH, max(1, int(val)))
-    return colors[i]
+class Sensor:
+    def __init__(self, args, width, height):
+        self.min_temp = args.min_temp
+        self.max_temp = args.max_temp
+
+        self.size = [int(width / 2), int(width / 2)]
+        self.pixels = [self.size[0] / 32, self.size[1] / 32]
+        self.start_pos = [int(width - self.size[0]), int((height - self.size[1]) / 2)]
+
+        self.colors = self.get_colors()
+
+        # pylint: disable=invalid-slice-index
+        self.points = [(math.floor(ix / 8), (ix % 8)) for ix in range(0, 64)]
+        self.grid_x, self.grid_y = np.mgrid[0:7:32j, 0:7:32j]
+        # pylint: enable=invalid-slice-index
+
+        # initialize the sensor
+        # self.sensor = adafruit_amg88xx.AMG88XX(i2c_bus)
+        self.sensor = None
+
+    def get_colors(self):
+        # the list of colors we can choose from
+        blue = Color("indigo")
+        red = Color("red")
+        colors = list(blue.range_to(red, COLORDEPTH))
+
+        # create the array of colors
+        return [
+            (int(c.red * 255), int(c.green * 255), int(c.blue * 255)) for c in colors
+        ]
+
+    def get_position(self, i, j):
+        pt1 = (
+            int((self.pixels[0] * i) + self.start_pos[0]),
+            int((self.pixels[1] * j) + self.start_pos[1]),
+        )
+        pt2 = (
+            int((self.pixels[0] * (i + 1)) + self.start_pos[0]),
+            int((self.pixels[1] * (j + 1)) + self.start_pos[1]),
+        )
+        return pt1, pt2
+
+    def get_color(self, v):
+        i = COLORDEPTH - min(COLORDEPTH, max(1, int(v)))
+        return self.colors[i]
+
+    def map_value(self, x, in_min, in_max, out_min, out_max):
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+    def draw(self, frame):
+        # read the pixels
+        pixels = []
+        # for row in sensor.pixels:
+        #     pixels = pixels + row
+        for temp in range(0, 64):
+            pixels.append(self.min_temp + (temp / 8))
+
+        pixels = [
+            self.map_value(p, self.min_temp, self.max_temp, 0, COLORDEPTH - 1)
+            for p in pixels
+        ]
+
+        # perform interpolation
+        bicubic = griddata(
+            self.points, pixels, (self.grid_x, self.grid_y), method="cubic"
+        )
+
+        # draw pixel
+        for i, row in enumerate(bicubic):
+            for j, pixel in enumerate(row):
+                pt1, pt2 = self.get_position(i, j)
+                color = self.get_color(pixel)
+
+                cv2.rectangle(
+                    frame, pt1, pt2, color, cv2.FILLED,
+                )
 
 
-def map_value(x, in_min, in_max, out_min, out_max):
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+def main():
+    args = parse_args()
 
+    # Get a reference to webcam #0 (the default one)
+    cap = cv2.VideoCapture(args.camera_id)
 
-while True:
-    # Grab a single frame of video
-    ret, frame = cap.read()
+    if args.width > 0 and args.height > 0:
+        frame_w = args.width
+        frame_h = args.height
+    else:
+        frame_w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        frame_h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-    # move video
-    w = int(SCREEN_W / 4)
-    M = [[1, 0, w], [0, 1, 0]]
+    print(frame_w, frame_h)
+    print('Press "Esc", "q" or "Q" to exit.')
 
-    h, w = frame.shape[:2]
-    M = np.float32(M)
-    frame = cv2.warpAffine(frame, M, (w, h))
+    # initialize the sensor
+    sensor = Sensor(args, frame_w, frame_h)
 
-    # read the pixels
-    pixels = []
-    # for row in sensor.pixels:
-    #     pixels = pixels + row
-    for temp in range(0, 64):
-        pixels.append(MINTEMP + (temp / 8))
+    while True:
+        # Grab a single frame of video
+        ret, frame = cap.read()
 
-    pixels = [map_value(p, MINTEMP, MAXTEMP, 0, COLORDEPTH - 1) for p in pixels]
+        # frame = cv2.resize("Video", (frame_w, frame_h), interpolation=cv2.INTER_AREA)
 
-    # perform interpolation
-    bicubic = griddata(points, pixels, (grid_x, grid_y), method="cubic")
+        # draw_video(frame)
 
-    # draw thermal
-    for i, row in enumerate(bicubic):
-        for j, pixel in enumerate(row):
-            pt1, pt2 = get_position(i, j)
-            color = get_color(pixel)
+        sensor.draw(frame)
 
-            cv2.rectangle(
-                frame, pt1, pt2, color, cv2.FILLED,
+        # frame = cv2.resizeWindow("Video", frame_w, frame_h)
+        # resized = cv2.resize("Video", (frame_w, frame_h), interpolation=cv2.INTER_AREA)
+        # cv2.imshow("resized", resized)
+
+        # Display the resulting image
+        cv2.imshow("Video", frame)
+
+        cv2.namedWindow("Video", cv2.WINDOW_NORMAL)
+
+        if args.full_screen:
+            cv2.setWindowProperty(
+                "Video", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN
             )
 
-    # border
-    cv2.rectangle(
-        frame, (0, 0), (w, BOX_POS[1]), BORDER, cv2.FILLED,
-    )
-    y2 = BOX_POS[1] + BOX_SIZE[1]
-    cv2.rectangle(
-        frame, (0, y2), (w, y2 + BOX_POS[1]), BORDER, cv2.FILLED,
-    )
+        ch = cv2.waitKey(1)
+        if ch == 27 or ch == ord("q") or ch == ord("Q"):
+            break
 
-    cv2.rectangle(
-        frame, (0, BOX_POS[1]), (BOX_SIZE[0], BOX_POS[1] + BOX_SIZE[1]), BORDER, 1,
-    )
-    cv2.rectangle(
-        frame,
-        (BOX_SIZE[0], BOX_POS[1]),
-        (BOX_SIZE[0] * 2, BOX_POS[1] + BOX_SIZE[1]),
-        BORDER,
-        1,
-    )
+    # Release handle to the webcam
+    cap.release()
+    cv2.destroyAllWindows()
 
-    # Display the resulting image
-    cv2.imshow("Video", frame)
 
-    # cv2.resizeWindow("Video", SCREEN_W, SCREEN_H)
-
-    cv2.namedWindow("Video", cv2.WINDOW_NORMAL)
-    cv2.setWindowProperty("Video", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
-
-# Release handle to the webcam
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    main()
