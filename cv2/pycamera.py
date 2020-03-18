088b351a0a2a7139e1b0b1eb8101d92bbc38be4e
nalbam <me@nalbam.com>
2020-03-18 19:24:22 +0900

    picamera

---
+++ cv2/pycamera.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import argparse
+import cv2
+
+from picamera import PiCamera
+
+camera = PiCamera()
+
+camera.start_preview()
+
+camera.exposure_mode = "beach"
+
+run = True
+while run:
+
+    ch = cv2.waitKey(1)
+    if ch == ord("c"):
+        camera.capture("/home/pi/beach.jpg")
+
+    if ch == 27 or ch == ord("q") or ch == ord("Q"):
+        break
+
+camera.stop_preview()
