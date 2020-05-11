df78b00e74b20e94d43bd46196cd94a17b4a7e5b
Jungyoul Yu <me@nalbam.com>
2020-05-11 11:59:18 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -209,7 +209,7 @@ apache() {
 
 node() {
     curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-    sudo apt install -y nodejs npm
+    sudo apt install -y nodejs
 
     _bar
     node -v
