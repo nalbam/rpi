c7b73f8cd58a3e9dd7f62c9c46027af852191fe9
nalbam <me@nalbam.com>
2021-02-12 01:08:40 +0900

    python3

--- aiy/voice/voice_player.py
+++ aiy/voice/voice_player.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3
+
 import argparse
 import time
 import threading
 
-from aiy.board import Board
+from aiy.board import Board, Led
 from aiy.voice.audio import AudioFormat, play_wav, record_file, Recorder
 
 
@@ -12,12 +14,16 @@ def main():
     args = parser.parse_args()
 
     with Board() as board:
+        board.led.state = Led.BEACON
+
         while True:
             print("Press button to play recorded sound.")
             board.button.wait_for_press()
 
             print("Playing...")
+            board.led.state = Led.ON
             play_wav(args.filename)
+            board.led.state = Led.BEACON
             print("Done.")
 
 
